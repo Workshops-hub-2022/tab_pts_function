9509198e3510582a0095b8fc9e9406e6cb375cb5
Nathan <nathan.barre@epitech.eu>
2022-11-16 23:51:16 +0100

    hide

--- src/tab_pts_func.c
+++ src/tab_pts_func.c
@@ -9,24 +9,4 @@
 
 void do_math(int a, int b, char action)
 {
-    char *controler = "+-*/^";
-    int index = 0;
-    int res = 0;
-    int (*tab[5])(int, int) = {
-        &addition,          // addition
-        &soustraction,      // soustraction
-        &multiplication,    // multiplication
-        &division,          // division
-        &power              // power
-    };
-
-    while (controler[index] != '\0'){
-        if (controler[index] == action) {
-            res = tab[index](a, b);
-            printf("Le resultat de %i %c %i = %i\n", a, action, b, res);
-            return;
-        }
-        index++;
-    }
-    printf("Operation inconnue\n");
 }
