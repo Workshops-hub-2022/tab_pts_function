60b4667b4a15265e0e3825308f819b1fcc356d6b
Nathan <nathan.barre@epitech.eu>
2022-11-17 10:52:30 +0100

    solution

--- src/math.c
+++ src/math.c
@@ -7,20 +7,33 @@
 
 int addition(int a, int b)
 {
+    return (a + b);
 }
 
 int soustraction(int a, int b)
 {
+    return (a - b);
 }
 
 int multiplication(int a, int b)
 {
+    return (a * b);
 }
 
 int division(int a, int b)
 {
+    return (a / b);
 }
 
 int power(int a, int b)
 {
+    int res = a;
+    int index = 1;
+
+    while (index != b) {
+        res = res * a;
+        index++;
+    }
+
+    return (res);
 }
--- src/tab_pts_func.c
+++ src/tab_pts_func.c
@@ -9,4 +9,25 @@
 
 void do_math(int a, int b, char action)
 {
+    int index = 0;
+    int res = 0;
+    char *controler = "+-*/^";
+
+    int (*tab[5])(int, int) = {
+        &addition,          // addition
+        &soustraction,      // soustraction
+        &multiplication,    // multiplication
+        &division,          // division
+        &power              // power
+    };
+
+    while (controler[index] != '\0'){
+        if (controler[index] == action) {
+            res = tab[index](a, b);
+            printf("Le resultat de %i %c %i = %i\n", a, action, b, res);
+            return;
+        }
+        index++;
+    }
+    printf("Operation inconnue\n");
 }
